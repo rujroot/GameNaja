0c2139837ce608927879ad07edc5479f9ddec724
PunnawitAck3003 <punnawitack@gmail.com>
2023-03-20 15:51:14 +0700

    Test Declare variable

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,5 +1,53 @@
 package entity;
 
 public abstract class Entity {
+	private String name;
 	private int hp;
+	private int atk;
+	private int def;
+	private int spd;
+	
+	public Entity(String name,int hp,int atk,int def,int spd){
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getAtk() {
+		return atk;
+	}
+
+	public void setAtk(int atk) {
+		this.atk = atk;
+	}
+
+	public int getDef() {
+		return def;
+	}
+	
+	public void setDef(int def) {
+		this.def = def;
+	}
+
+	public int getSpd() {
+		return spd;
+	}
+
+	public void setSpd(int spd) {
+		this.spd = spd;
+	}	
 }
--- src/entity/Zombie.java
+++ src/entity/Zombie.java
@@ -2,4 +2,9 @@ package entity;
 
 public class Zombie extends Entity {
 
+	public Zombie(String name, int hp, int atk, int def, int spd) {
+		super(name, hp, atk, def, spd);
+		// TODO Auto-generated constructor stub
+	}
+
 }
