3ce3e04c9d87633054809c468a67cc1efbb336b4
rujroot <ruj9root@gmail.com>
2023-03-20 17:07:49 +0700

    add Hitbox

--- src/equipment/BaseWeapon.java
+++ src/equipment/BaseWeapon.java
@@ -1,17 +1,49 @@
 package equipment;
 
+import entity.Entity;
+import logic.Hitbox;
+
 public abstract class BaseWeapon {
-	private int attack;
+	private double attackDamage;
+	private double attackSpeed;
+	private Hitbox attackHitBox;
 	
-	public BaseWeapon(int attack) {
-		this.setAttack(attack);
+	public abstract void attack(Entity entity);
+	
+	public BaseWeapon(int attackDamage, int attackSpeed) {
+		this.setAttackDamage(attackDamage);
+		this.setAttackSpeed(attackSpeed);
+	}
+	
+	public BaseWeapon(int attackDamage) {
+		this.setAttackDamage(attackDamage);
+		this.setAttackSpeed(1);
 	}
 
-	public int getAttack() {
-		return attack;
+	public double getAttackDamage() {
+		return attackDamage;
 	}
 
-	public void setAttack(int attack) {
-		this.attack = attack;
+	public void setAttackDamage(double attackDamage) {
+		this.attackDamage = attackDamage;
 	}
+
+	public double getAttackSpeed() {
+		return attackSpeed;
+	}
+
+	public void setAttackSpeed(double attackSpeed) {
+		this.attackSpeed = attackSpeed;
+	}
+
+	public Hitbox getAttackHitBox() {
+		return attackHitBox;
+	}
+
+	public void setAttackHitBox(Hitbox attackHitBox) {
+		this.attackHitBox = attackHitBox;
+	}
+
+	
+
 }
