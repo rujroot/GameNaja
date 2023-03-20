f1874213b15ef209eb6f191b9d06509104f0dbe0
rujroot <ruj9root@gmail.com>
2023-03-20 16:54:39 +0700

    add Hitbox

---
+++ src/Math/Point.java
@@ -0,0 +1,22 @@
+package Math;
+
+public class Point {
+	private double X, Y;
+	public Point(double X, double Y) {
+		this.setX(X);
+		this.setY(Y);
+	}
+	public double getX() {
+		return X;
+	}
+	public void setX(double x) {
+		X = x;
+	}
+	public double getY() {
+		return Y;
+	}
+	public void setY(double y) {
+		Y = y;
+	}
+
+}
--- src/logic/Hitbox.java
+++ src/logic/Hitbox.java
@@ -1,44 +1,68 @@
 package logic;
 
+import Math.Point;
+
 public class Hitbox {
-	private int centerPoX, centerPoY, lenght, wide;
+	private Point Center;
+	private double length, width;
 	
-	public Hitbox(int centerPoX, int centerPoY, int lenght, int wide) {
-		this.setCenterPoX(centerPoX);
-		this.setCenterPoY(centerPoY);
-		this.setLenght(lenght);
-		this.setWide(wide);
+	public Hitbox(Point Center, double length, double width) {
+		this.setCenter(Center);
+		this.setLength(length);
+		this.setWidth(width);
+	}
+	
+	public Point getTopleft() {
+		double X = Center.getX() - (width / 2);
+		double Y = Center.getY() - (length / 2);
+		Point TL = new Point(X, Y);
+		return TL;
+	}
+	
+	public boolean isIntersect(Hitbox A, Hitbox B) {
+		Point TLA = A.getTopleft();
+		double lengthA = A.getLength();
+		double widthA = A.getWidth();
+		
+		Point TLB = B.getTopleft();
+		double lengthB = B.getLength();
+		double widthB = B.getWidth();
+		
+		// Intersect X
+		if(Math.min( TLA.getX() + widthA, TLB.getX() + widthB) >= Math.max(TLA.getX(), TLB.getX())) {
+			// Intersect Y
+			if(Math.min( TLA.getY() + lengthA, TLB.getY() + lengthB) >= Math.max(TLA.getY(), TLB.getY())) {
+				return true;
+			}
+			return false;
+		}
+		return false;
+		
+			
+	}
+	
+	public double getLength() {
+		return length;
 	}
 
-	public int getCenterPoX() {
-		return centerPoX;
+	public void setLength(double length) {
+		this.length = length;
 	}
 
-	public void setCenterPoX(int centerPoX) {
-		this.centerPoX = centerPoX;
+	public double getWidth() {
+		return width;
 	}
 
-	public int getCenterPoY() {
-		return centerPoY;
+	public void setWidth(double width) {
+		this.width = width;
 	}
 
-	public void setCenterPoY(int centerPoY) {
-		this.centerPoY = centerPoY;
+	public Point getCenter() {
+		return Center;
 	}
 
-	public int getLenght() {
-		return lenght;
+	public void setCenter(Point center) {
+		Center = center;
 	}
 
-	public void setLenght(int lenght) {
-		this.lenght = lenght;
-	}
-
-	public int getWide() {
-		return wide;
-	}
-
-	public void setWide(int wide) {
-		this.wide = wide;
-	}
 }
