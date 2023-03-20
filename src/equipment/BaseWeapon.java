a72f99381bcaa773144043f0115f21d9964814b1
PunnawitAck3003 <punnawitack@gmail.com>
2023-03-20 15:53:56 +0700

    Test Variable

--- src/equipment/BaseWeapon.java
+++ src/equipment/BaseWeapon.java
@@ -1,5 +1,19 @@
 package equipment;
 
-public class BaseWeapon {
+public abstract class BaseWeapon {
+	private int attack;
+	
+	public BaseWeapon(int attack) {
+		this.setAttack(attack);
+	}
 
+	public int getAttack() {
+		return attack;
+	}
+
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+	
+	
 }
