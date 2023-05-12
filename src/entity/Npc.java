7623658307d4380e6165a57e89e3f798e2eea26c
PunnawitAck3003 <punnawitack@gmail.com>
2023-05-12 21:54:34 +0700

    ganbatte John

--- src/entity/Npc.java
+++ src/entity/Npc.java
@@ -14,134 +14,149 @@ import logic.RenderableHolder;
 
 public class Npc extends Entity implements Cooldownable {
 
-    private double cooldownTime = 1000;
+	private double cooldownTime = 1000;
 	private double lastClickTime = 0;
 
-    private WritableImage image = new WritableImage(RenderableHolder.Tileset.getPixelReader(), 644, 964, 59, 59);
-    private Entity followEntity;
-    private double maxDistance = 200 ;
-    private BaseWeapon equipment;
+	private WritableImage image = new WritableImage(RenderableHolder.Tileset.getPixelReader(), 644, 964, 59, 59);
+	private Entity followEntity;
+	private double maxDistance = 200;
+	private BaseWeapon equipment;
 
-    public Npc(String name, double width, double height, DataEntity data) {
-        super(name, width, height, data);
-        this.setWidth(image.getWidth());
-        this.setHeight(image.getHeight());
+	public Npc(String name, double width, double height, DataEntity data) {
+		super(name, width, height, data);
+		this.setWidth(image.getWidth());
+		this.setHeight(image.getHeight());
 
-        this.setEquipment(new Knife(10, 10));
-    }
+		this.setEquipment(new Knife(10, 10));
+	}
 
-    @Override
-    public void draw(GraphicsContext gc) {
-        Point pos = this.getPosition();
+	@Override
+	public void draw(GraphicsContext gc) {
+		Point pos = this.getPosition();
 		gc.drawImage(image, pos.getX(), pos.getY(), image.getWidth(), image.getHeight());
 		this.drawHP(gc);
 
-        if (equipment != null) {
+		if (equipment != null) {
 			equipment.draw(gc);
 		}
-    }
+	}
 
-    @Override
-    public void attack() {
-        if(followEntity.equals(Player.getPlayer()) || onCooldown()) return;
-        equipment.attack();
-    }
+	@Override
+	public void attack() {
+		if (followEntity.equals(Player.getPlayer()) || onCooldown())
+			return;
+		equipment.attack();
+	}
 
-    public void doBehavior(){
-        follow(followEntity);
-        attack();
-    }
+	public void doBehavior() {
+		follow(followEntity);
+		attack();
+	}
 
-    public void follow(Entity entity) {
+	public void follow(Entity entity) {
 		Point pp = entity.getPosition();
 
 		double px = pp.getX(), py = pp.getY();
 
-		Point p = new Point(this.getPosition().getX() - px - 10, this.getPosition().getY() - py - 10);
+		Point p = new Point(this.getPosition().getX() - px - 0, this.getPosition().getY() - py - 0);
 		double distance = Math.sqrt(p.getX() * p.getX() + p.getY() * p.getY());
 
 		DataEntity data = this.getData();
 
 		if (distance > 0) {
-			this.move(-p.getX() / distance * data.getSpd(), 0);
-			this.move(0, -p.getY() / distance * data.getSpd());
+			double mx = -p.getX() / distance * data.getSpd();
+			double my = -p.getY() / distance * data.getSpd();
+			// Check for obstacles
+			if (!this.isLegalMove(mx, my)) {
+				// Turn 90 degrees
+				double newX = -p.getY();
+				double newY = p.getX();
+				double newDistance = Math.sqrt(newX * newX + newY * newY);
+				this.move(newX / newDistance * data.getSpd(), 0);
+                this.move(0, newY / newDistance * data.getSpd());
+				//this.move(newX / newDistance * data.getSpd(), newY / newDistance * data.getSpd());
+			} else {
+				// Move towards target
+				this.move(-p.getX() / distance * data.getSpd(), 0);
+                this.move(0, -p.getY() / distance * data.getSpd());
+				//this.move(-p.getX() / distance * data.getSpd(), -p.getY() / distance * data.getSpd());
+			}
 		}
 
+	}// this.isLegalMove( mx, my)
+
+	public double distance(Point pos1) {
+		Point pos2 = this.getPosition();
+		return Math.sqrt(Math.pow(pos1.getX() - pos2.getX(), 2) + Math.pow(pos1.getY() - pos2.getY(), 2));
 	}
 
-    public double distance(Point pos1){
-        Point pos2 = this.getPosition();
-        return Math.sqrt(Math.pow(pos1.getX() - pos2.getX(), 2) + Math.pow(pos1.getY() - pos2.getY(), 2));
-    }
+	public void findNearestMonster(ArrayList<BaseObject> gameObjectContainer) {
+		Entity nearestMonster = Player.getPlayer();
+		double minDistance = 1e9;
 
-    public void findNearestMonster(ArrayList<BaseObject> gameObjectContainer){
-        Entity nearestMonster = Player.getPlayer();
-        double minDistance = 1e9;
-
-        for (int i = gameObjectContainer.size() - 1; i >= 0; i--) {
+		for (int i = gameObjectContainer.size() - 1; i >= 0; i--) {
 			BaseObject object = gameObjectContainer.get(i);
-			if(object instanceof Monster && distance( object.getPosition()) <= maxDistance){
-                if(distance( object.getPosition()) <= minDistance){
-                    nearestMonster = (Entity) object;
-                    minDistance = distance( object.getPosition());
-                }
-            }
-        }
+			if (object instanceof Monster && distance(object.getPosition()) <= maxDistance) {
+				if (distance(object.getPosition()) <= minDistance) {
+					nearestMonster = (Entity) object;
+					minDistance = distance(object.getPosition());
+				}
+			}
+		}
 
-        followEntity = nearestMonster;
-    }
+		followEntity = nearestMonster;
+	}
 
-    public double getEntityAngle(){
+	public double getEntityAngle() {
 
-        Point pos = this.getPosition();
+		Point pos = this.getPosition();
 
 		double posX = followEntity.getPosition().getX();
-		double posY = followEntity.getPosition().getY();;
+		double posY = followEntity.getPosition().getY();
+		;
 
 		double startAt = Math.atan2(posY - (pos.getY() + this.getHeight() / 2),
-				                    posX - (pos.getX() + this.getWidth() / 2));
+				posX - (pos.getX() + this.getWidth() / 2));
 		if (startAt < 0) {
 			startAt += 2 * Math.PI;
 		}
 		return 360 - Math.toDegrees(startAt);
-    }
-    
-    public Entity getFollowEntity() {
-        return followEntity;
-    }
+	}
 
-    public void setFollowEntity(Entity followEntity) {
-        this.followEntity = followEntity;
-    }
+	public Entity getFollowEntity() {
+		return followEntity;
+	}
 
-    public double getMaxDistance() {
-        return maxDistance;
-    }
+	public void setFollowEntity(Entity followEntity) {
+		this.followEntity = followEntity;
+	}
 
-    public void setMaxDistance(double maxDistance) {
-        this.maxDistance = maxDistance;
-    }
+	public double getMaxDistance() {
+		return maxDistance;
+	}
 
-    public BaseWeapon getEquipment() {
-        return equipment;
-    }
+	public void setMaxDistance(double maxDistance) {
+		this.maxDistance = maxDistance;
+	}
 
-    public void setEquipment(BaseWeapon equipment) {
-        this.equipment = equipment;
-        equipment.setEntity(this);
-    }
+	public BaseWeapon getEquipment() {
+		return equipment;
+	}
 
-    @Override
-    public boolean onCooldown() {
-        long currentTime = System.currentTimeMillis();
+	public void setEquipment(BaseWeapon equipment) {
+		this.equipment = equipment;
+		equipment.setEntity(this);
+	}
+
+	@Override
+	public boolean onCooldown() {
+		long currentTime = System.currentTimeMillis();
 		if (currentTime - lastClickTime > cooldownTime) {
 			lastClickTime = currentTime;
 			return false;
 		} else {
 			return true;
 		}
-    }
+	}
 
-    
-    
 }
