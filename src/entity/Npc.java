5f7d51e0914cb94cf446995d40bb38da439ba9a7
rujroot <ruj9root@gmail.com>
2023-05-20 14:14:00 +0700

    setnpc

--- src/entity/Npc.java
+++ src/entity/Npc.java
@@ -31,7 +31,7 @@ public class Npc extends Entity implements Cooldownable {
 	private double maxDistance = 500;
 	private BaseWeapon equipment;
 	private String state = "Idel";
-	private double value = 50;
+	private double value = 25;
 
 	public Npc(String name, double width, double height, DataEntity data) {
 		super(name, width, height, data);
